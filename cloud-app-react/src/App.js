f53cffe6d39f17357afb3c82e000fe0f28272696
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-08 11:00:57 +0600

    add heroicons and navbar

--- cloud-app-react/src/App.js
+++ cloud-app-react/src/App.js
@@ -1,9 +1,12 @@
+import Navbar from "./components/Navbar";
+
 
 
 function App() {
   return (
     <>
-     <h1 className="text-3xl font-bold underline">Hello</h1>
+  
+     <Navbar/>
     </>
   );
 }
