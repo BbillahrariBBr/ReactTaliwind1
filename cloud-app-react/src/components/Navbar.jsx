124d6f0cf58146ec9da2e3885c5febc9cac6d94d
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-08 11:16:06 +0600

    butto add

--- cloud-app-react/src/components/Navbar.jsx
+++ cloud-app-react/src/components/Navbar.jsx
@@ -7,6 +7,18 @@ const Navbar = () => {
             <div className='px-2 flex justify-between items-center w-full h-full'>
                 <div className='flex items-center'>
                     <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Brand.</h1>
+                    <ul className="hidden md:flex">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Support</li>
+                        <li>Platforms</li>
+                        <li>Pricing</li>
+                    </ul>
+
+                </div>
+                <div className='hidden md:flex pr-4'>
+                    <button className='border-none bg-transparent text-black mr-4'>Sign in</button>
+                    <button className='px-8 py-3'>Sign Up</button>
 
                 </div>
             </div>
