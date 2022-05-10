13d3245c0e5f332ae273ef6cc513f1517ea3f117
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-10 10:05:16 +0600

    screen resuation add

--- cloud-app-react/tailwind.config.js
+++ cloud-app-react/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    sm:'640px',
+    // => @medida (min width 640 px)
+    md:'768px',
+    // => @medida (min width 768 px)
+    lg:'1024px',
+    // => @medida (min width 1024 px)
+    xl:'1280px',
+    // => @medida (min width 1280 px)
+    '2xl':'1536px',
+    // => @medida (min width 1536 px)
     extend: {},
   },
   plugins: [],
