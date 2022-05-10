7d876f1b7530da8b6055b60bd4cb9d7ba5c375cb
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-10 10:48:21 +0600

    hamburger menu almost complete

--- cloud-app-react/tailwind.config.js
+++ cloud-app-react/tailwind.config.js
@@ -3,16 +3,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    sm:'640px',
-    // => @medida (min width 640 px)
-    md:'768px',
-    // => @medida (min width 768 px)
-    lg:'1024px',
-    // => @medida (min width 1024 px)
-    xl:'1280px',
-    // => @medida (min width 1280 px)
-    '2xl':'1536px',
-    // => @medida (min width 1536 px)
+    sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     extend: {},
   },
   plugins: [],
